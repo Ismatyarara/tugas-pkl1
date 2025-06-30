582a9dfed444f2f3e5594ff45562175d723fe510
ismatyarara <isma51@gmail.com>
2025-06-30 10:30:10 +0530

    tugas10

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ProductController;
+//import
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//auth Routes
+Route::post('register', [AuthController::class, 'register']);
+
+Route::post('login', [AuthController::class, 'login']);
+
+//wajib login
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('logout', [AuthController::class, 'logout']);
+   //dibawah ini akan di isi route lain
+    Route::get('products',[ProductController::class, 'index']);
+    Route::get('products/{id}',[ProductController::class, 'show']);
 });
